1c2558ddf7f2ceeb66303bed97d750b4eb05e13e
Freivin Campbell <freivin12@gmail.com>
2021-09-13 20:35:37 -0600

    Type aliases & definitions
    Type aliases & definitions

--- Creating Custom Data Types with Go/datatypes/main.go
+++ Creating Custom Data Types with Go/datatypes/main.go
@@ -8,10 +8,13 @@ import (
 func main() {
 	p := organization.NewPerson("Freivin", "Campbell")
 	err := p.SetTwitterHandler("@freivincampbell")
+	fmt.Printf("%T\n", organization.TwitterHandler("test"))
 	if err != nil {
 		fmt.Printf("an error occurred setting twitter handeler: %s\n", err.Error())
 	}
+
 	println(p.TwitterHandler())
+	println(p.TwitterHandler().RedirectUrl())
 	println(p.ID())
 	println(p.FullName())
 }
--- Creating Custom Data Types with Go/datatypes/organization/person.go
+++ Creating Custom Data Types with Go/datatypes/organization/person.go
@@ -6,6 +6,19 @@ import (
 	"strings"
 )
 
+type Handler struct {
+	handle string
+	name string
+}
+
+type TwitterHandler string
+
+func (th TwitterHandler) RedirectUrl() string {
+	cleanHandler := strings.TrimPrefix(string(th), "@")
+
+	return fmt.Sprintf("https://www.twitter.com/%s", cleanHandler)
+}
+
 type Identifiable interface {
 	ID() string
 }
@@ -13,7 +26,7 @@ type Identifiable interface {
 type Person struct {
 	firstName      string
 	lastName       string
-	twitterHandler string
+	twitterHandler TwitterHandler
 }
 
 func NewPerson(fistName, lastName string) Person {
@@ -31,10 +44,10 @@ func (p *Person) ID() string {
 	return "12345"
 }
 
-func (p *Person) SetTwitterHandler(handler string) error {
+func (p *Person) SetTwitterHandler(handler TwitterHandler) error {
 	if len(handler) == 0 {
 		p.twitterHandler = handler
-	} else if !strings.HasPrefix(handler, "@") {
+	} else if !strings.HasPrefix(string(handler), "@") {
 		return errors.New("twitter handler must start with an @ symbol")
 	}
 
@@ -42,6 +55,6 @@ func (p *Person) SetTwitterHandler(handler string) error {
 	return nil
 }
 
-func (p *Person) TwitterHandler() string {
+func (p *Person) TwitterHandler() TwitterHandler {
 	return p.twitterHandler
 }
